d1002b28128c5439618563810a675c4b97313a19
codeliner <kontakt@codeliner.ws>
2024-03-19 19:39:40 +0100

    fix: endless loop of query deps

--- precache-manifest.f5b5fb116968feeac75c092de80f59e3.js
+++ precache-manifest.f5b5fb116968feeac75c092de80f59e3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8c9894404f8b4239c735",
+    "revision": "2ac25bf8dd6776bf0afe",
     "url": "https://free.prooph-board.com/static/css/main.5e88d10e.chunk.css"
   },
   {
-    "revision": "8c9894404f8b4239c735",
-    "url": "https://free.prooph-board.com/static/js/main.8c989440.chunk.js"
+    "revision": "2ac25bf8dd6776bf0afe",
+    "url": "https://free.prooph-board.com/static/js/main.2ac25bf8.chunk.js"
   },
   {
     "revision": "0c44632a8928349f543d",
@@ -36,16 +36,16 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/icons.faff9214.woff"
   },
   {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "https://free.prooph-board.com/static/media/icons.b87b9ba5.ttf"
+  },
+  {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "https://free.prooph-board.com/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "cd6c777f1945164224dee082abaea03a",
     "url": "https://free.prooph-board.com/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "https://free.prooph-board.com/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
@@ -60,12 +60,16 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/outline-icons.82f60bd0.svg"
   },
   {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "https://free.prooph-board.com/static/media/brand-icons.e8c322de.woff2"
-  },
-  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "https://free.prooph-board.com/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "https://free.prooph-board.com/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "https://free.prooph-board.com/static/media/brand-icons.e8c322de.woff2"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -76,10 +80,6 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "https://free.prooph-board.com/static/media/icons.962a1bf3.svg"
-  },
-  {
     "revision": "a1a749e89f578a49306ec2b055c073da",
     "url": "https://free.prooph-board.com/static/media/brand-icons.a1a749e8.svg"
   },
@@ -88,7 +88,7 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/cody.63c5836d.svg"
   },
   {
-    "revision": "b1cf28c5e1e9b07aaae39c5b7fdc3664",
+    "revision": "62a4a2574c8256cc579c32f16b8beeed",
     "url": "https://free.prooph-board.com/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://free.prooph-board.com/precache-manifest.216ed44f37a7deeba7239aafb06d02d9.js"
+  "https://free.prooph-board.com/precache-manifest.f5b5fb116968feeac75c092de80f59e3.js"
 );
 
 workbox.clientsClaim();
