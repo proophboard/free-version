ae118087485bf91a8b39883f09e000bef66f0903
codeliner <kontakt@codeliner.ws>
2025-04-03 20:07:41 +0200

    Fix: stop pinch zoom on wheel event

--- precache-manifest.549d33c846a9ccbf657f6c76c83f3658.js
+++ precache-manifest.549d33c846a9ccbf657f6c76c83f3658.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9517bc7178d1daf1b3e0",
+    "revision": "422566d7585d2a81aa46",
     "url": "https://free.prooph-board.com/static/css/main.84fbc54f.chunk.css"
   },
   {
-    "revision": "9517bc7178d1daf1b3e0",
-    "url": "https://free.prooph-board.com/static/js/main.9517bc71.chunk.js"
+    "revision": "422566d7585d2a81aa46",
+    "url": "https://free.prooph-board.com/static/js/main.422566d7.chunk.js"
   },
   {
     "revision": "321f66119bafbfb7a890",
@@ -28,16 +28,20 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "https://free.prooph-board.com/static/media/icons.b87b9ba5.ttf"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "https://free.prooph-board.com/static/media/icons.8e3c7f55.eot"
   },
   {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "https://free.prooph-board.com/static/media/icons.faff9214.woff"
+  },
+  {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "https://free.prooph-board.com/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "https://free.prooph-board.com/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "cd6c777f1945164224dee082abaea03a",
@@ -52,10 +56,6 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "https://free.prooph-board.com/static/media/outline-icons.82f60bd0.svg"
-  },
-  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "https://free.prooph-board.com/static/media/brand-icons.13db00b7.eot"
   },
@@ -64,31 +64,31 @@ self.__precacheManifest = [
     "url": "https://free.prooph-board.com/static/media/brand-icons.e8c322de.woff2"
   },
   {
-    "revision": "a046592bac8f2fd96e994733faf3858c",
-    "url": "https://free.prooph-board.com/static/media/brand-icons.a046592b.woff"
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "https://free.prooph-board.com/static/media/outline-icons.82f60bd0.svg"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "https://free.prooph-board.com/static/media/icons.962a1bf3.svg"
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "https://free.prooph-board.com/static/media/brand-icons.a046592b.woff"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "https://free.prooph-board.com/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "a1a749e89f578a49306ec2b055c073da",
-    "url": "https://free.prooph-board.com/static/media/brand-icons.a1a749e8.svg"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "https://free.prooph-board.com/static/media/icons.962a1bf3.svg"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "https://free.prooph-board.com/static/media/icons.faff9214.woff"
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "https://free.prooph-board.com/static/media/brand-icons.a1a749e8.svg"
   },
   {
     "revision": "63c5836d434b489620d45326605cb59c",
     "url": "https://free.prooph-board.com/static/media/cody.63c5836d.svg"
   },
   {
-    "revision": "9f4d6fe1d407ca45c53af93a4ca6e029",
+    "revision": "e5b18bd7f3fb2154e6e7a59b28e0dc4c",
     "url": "https://free.prooph-board.com/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://free.prooph-board.com/precache-manifest.8e73493cb0adada65fa663de6a0ec685.js"
+  "https://free.prooph-board.com/precache-manifest.549d33c846a9ccbf657f6c76c83f3658.js"
 );
 
 workbox.clientsClaim();
